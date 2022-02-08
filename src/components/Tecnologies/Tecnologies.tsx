02f10bb46c3abc8b121d4341a8686dbb26d6f195
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-07 23:33:31 -0300

    title added and some margin fixed

--- src/components/Tecnologies/Tecnologies.tsx
+++ src/components/Tecnologies/Tecnologies.tsx
@@ -4,13 +4,18 @@ import image1 from "../../assets/images/iconos/icons8-python-144.png";
 
 const Tecnologies = () => {
   return (
-    <div className="overflow-hidden flex  justify-center items-center ">
-      {tecOps.map((tec, index: number) => (
-        <div className="" key={index}>
-          <img className="mx-20 w-20" src={tec.logo} alt={tec.name} />
-          <h4 className="mx-20 w-20 text-center">{tec.name}</h4>
-        </div>
-      ))}
+    <div className=" w-[calc(100%-40px)] m-10 ">
+      <h2 className="text-5xl font-bold text-center mb-10">
+        Tecnologias que utilizo
+      </h2>
+      <div className="flex flex-wrap justify-center items-center">
+        {tecOps.map((tec, index: number) => (
+          <div className="mb-10" key={index}>
+            <img className="mx-20 w-20 " src={tec.logo} alt={tec.name} />
+            <h4 className="mx-20 w-20 text-center">{tec.name}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
