5a47996de93037fec2d681aeb5235720c502d1ec
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-09 19:56:59 -0300

    Scroll animation added with tailwind css

--- src/components/Tecnologies/Tecnologies.tsx
+++ src/components/Tecnologies/Tecnologies.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { tecOps } from "./tecops";
-import "./index.css";
 
 const Tecnologies = () => {
   return (
@@ -8,21 +7,15 @@ const Tecnologies = () => {
       <h2 className="text-5xl px-10 py-5 font-bold text-center mb-10">
         Tecnologias que utilizo
       </h2>
-      <div className="slider">
-        <div className="slider-track">
+      <div className="bg-white h-auto m-0 overflow-hidden relative w-full before:slider-before after:slider-after">
+        <div className="slider-track flex w-[calc(250px * 4)] animate-scroll  ">
           {tecOps.map((tec, index: number) => (
             //  mb-10
-            <div className="slide w-30" key={index}>
+            <div className="slide w-screen mx-3 md:mx-5 lg:mx-11" key={index}>
               {/* mx-20  */}
-              <img
-                className="w-20 mx-3 sm:mx-10 lg:mx-35"
-                src={tec.logo}
-                alt={tec.name}
-              />
+              <img className="w-20 " src={tec.logo} alt={tec.name} />
               {/* mx-20 */}
-              <h4 className=" w-20 mx-3 sm:mx-10 md:mx-35 text-center">
-                {tec.name}
-              </h4>
+              <h4 className=" w-20 text-center">{tec.name}</h4>
             </div>
           ))}
         </div>
