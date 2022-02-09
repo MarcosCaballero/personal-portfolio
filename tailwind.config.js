3b3ffbe1b578c2082545d39bbd534106142084d5
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-09 19:56:17 -0300

    scroll animation and keyframe added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}", "./.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(calc(-250px * 2))",
+          },
+        },
+      },
+      animation: {
+        scroll: "scroll 15s ease-out infinite",
+      },
+    },
   },
   plugins: [],
 };
