27a70d031fe0fdc116938c9e7b1206f9b9b2ac12
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-08 23:21:12 -0300

    inifinity carrousel with pure css
    On the following push I'm going to convert all the pure css to tailwind css and delete the index.css file

--- src/components/Tecnologies/Tecnologies.tsx
+++ src/components/Tecnologies/Tecnologies.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { tecOps } from "./tecops";
-import image1 from "../../assets/images/iconos/icons8-python-144.png";
+import "./index.css";
 
 const Tecnologies = () => {
   return (
-    <div className=" w-[calc(100%-40px)] m-10 ">
-      <h2 className="text-5xl font-bold text-center mb-10">
+    <div className=" w-100% ">
+      <h2 className="text-5xl px-10 py-5 font-bold text-center mb-10">
         Tecnologias que utilizo
       </h2>
-      <div className="flex flex-wrap justify-center items-center">
-        {tecOps.map((tec, index: number) => (
-          <div className="mb-10" key={index}>
-            <img className="mx-20 w-20 " src={tec.logo} alt={tec.name} />
-            <h4 className="mx-20 w-20 text-center">{tec.name}</h4>
-          </div>
-        ))}
+      <div className="slider">
+        <div className="slider-track">
+          {tecOps.map((tec, index: number) => (
+            //  mb-10
+            <div className="slide w-30" key={index}>
+              {/* mx-20  */}
+              <img
+                className="w-20 mx-3 sm:mx-10 lg:mx-35"
+                src={tec.logo}
+                alt={tec.name}
+              />
+              {/* mx-20 */}
+              <h4 className=" w-20 mx-3 sm:mx-10 md:mx-35 text-center">
+                {tec.name}
+              </h4>
+            </div>
+          ))}
+        </div>
       </div>
+      {/* <div className="flex flex-wrap justify-center items-center"></div> */}
     </div>
   );
 };
