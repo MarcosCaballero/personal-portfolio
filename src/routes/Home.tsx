833009faa64eec2e78ca279e9b0a0659849dd19d
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-09 23:42:00 -0300

    componente Landing y Projects añadidos

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import About from "../components/About/About";
+import Landing from "../components/Landing/Landing";
 import Navbar from "../components/Navbar/Navbar";
 import Tecnologies from "../components/Tecnologies/Tecnologies";
+import Projects from "../components/Projects/Projects";
 
 const Home = () => {
   return (
     <div className="bg-gray-200 h-screen">
       <Navbar />
-      <About />
+      <Landing />
       <Tecnologies />
+      <Projects />
     </div>
   );
 };
