8ab717b42fadf9b2ffbd51a14ac53fa4d83c18c8
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-09 23:15:29 -0300

    added more high and padding

--- src/components/Tecnologies/Tecnologies.tsx
+++ src/components/Tecnologies/Tecnologies.tsx
@@ -7,10 +7,13 @@ const Tecnologies = () => {
       <h2 className="text-5xl px-10 py-5 font-bold text-center mb-10">
         Tecnologías que utilizo
       </h2>
-      <div className="bg-white h-auto m-0 overflow-hidden relative w-full before:slider-before after:slider-after">
+      <div className="bg-white h-50 m-0 py-5 overflow-hidden relative w-full before:slider-before after:slider-after">
         <div className="slider-track flex w-[calc(250px * 4)] animate-scroll  ">
           {tecOps.map((tec, index: number) => (
-            <div className="slide w-screen mx-3 md:mx-5 lg:mx-11" key={index}>
+            <div
+              className="slide w-screen mx-3 md:mx-5 lg:mx-11 hover:scale-110"
+              key={index}
+            >
               <img className="w-20 " src={tec.logo} alt={tec.name} />
               <h4 className=" w-20 text-center">{tec.name}</h4>
             </div>
